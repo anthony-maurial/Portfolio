cd25c9ed6f3bdcfdb275b7e711332049faf82c31
anthony-maurial <157287952+anthony-maurial@users.noreply.github.com>
2025-06-09 21:49:54 +0200

    mon Portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,25 @@
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3')
+let p4 = document.getElementById('p4')
+let p5 = document.getElementById('p5')
+
+p1.addEventListener('click', () => {
+  window.location.href = 'projet1.html';
+});
+
+p2.addEventListener('click', () => {
+  window.location.href = 'projet2.html';
+});
+
+p3.addEventListener('click', () => {
+  window.location.href = 'projet3.html';
+});
+
+p4.addEventListener('click', () => {
+  window.location.href = 'projet4.html';
+});
+
+p5.addEventListener('click', () => {
+  window.location.href = 'projet5.html';
+});
